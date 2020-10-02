d7a9c89e1782b5b1797d7f88d72b21b505f91364
Thomas Campistron <irevoire@hotmail.fr>
2020-10-02 17:54:44 +0200

    simplify the project architecture

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-mod tree;
-
 use std::io::{self, BufRead};
 
-
 fn main() {
-    let mut tree = tree::TreeWord::new("FirstGraph".to_string());
+    let mut tree = word_tree::TreeWord::new("FirstGraph".to_string());
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         match line {
             Ok(word) => tree.add_word(&word),
-            Err(_) => break
+            Err(_) => break,
         }
     }
     tree.to_graph();
